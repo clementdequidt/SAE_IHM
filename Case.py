2e3babaecbdf62fbda4fbcb8de7c04d60539b4d9
Cousin Bastien <bastiencousin16@gmail.com>
2025-06-10 17:59:21 +0200

    Ajout de commentaires temporaire pour décrire l'état d'avancée de chaque fonction programmée / en cours

--- Case.py
+++ Case.py
@@ -1,5 +1,5 @@
 class Case():
-    
+    # OK
     def __init__(self, coord: tuple, depart: bool = False, caisse: bool = False, rayon: bool = False, itemsInRayon: list = [], obstacle: bool = False):
         self.__coord = coord
         self.__depart = depart
@@ -9,44 +9,58 @@ class Case():
         self.__itemsAccessible: list = []
         self.__obstacle = obstacle
     
+    # OK
     def getCoord(self):
         return self.__coord
     
+    # OK
     def isDepart(self):
         return self.__depart
     
+    # OK
     def isCaisse(self):
         return self.__caisse
     
+    # OK
     def isRayon(self):
         return self.__rayon
     
+    # OK
     def getItemsInRayon(self):
         return self.__itemsInRayon
     
+    # OK
     def getItemsAccessible(self):
         return self.__itemsAccessible
     
+    # OK
     def isObstacle(self):
         return self.__obstacle
     
+    # OK
     def setCoord(self, coord: tuple):
         self.__coord = coord
         
+    # OK
     def setDepart(self, depart: bool):
         self.__depart = depart
-        
+    
+    # OK
     def setCaisse(self, caisse: bool):
         self.__caisse = caisse
-        
+    
+    # OK
     def setRayon(self, rayon: bool):
         self.__rayon = rayon
     
+    # OK
     def setItemsInRayon(self, itemsInRayon: list):
         self.__itemsInRayon = itemsInRayon
-        
+    
+    # OK
     def setItemsAccessible(self, itemsAccessible: list):
         self.__itemsAccessible = itemsAccessible
     
+    # OK
     def setObstacle(self, obstacle: bool):
         self.__obstacle = obstacle
--- Plan.py
+++ Plan.py
@@ -2,30 +2,38 @@ from Case import Case
 import heapq
 
 class Plan():
+    # OK
     def __init__(self, longueur: int, largeur: int):
         self.__longueur = longueur
         self.__largeur = largeur
         self.__cases = [[Case((i, j)) for i in range(longueur)] for j in range(largeur)]
     
+    # OK
     def getLongueur(self):
         return self.__longueur
     
+    # OK
     def getLargeur(self):
         return self.__largeur
     
+    # OK
     def getCase(self, coord: tuple):
         if 0 <= coord[0] < self.__largeur and 0 <= coord[1] < self.__longueur:
             return self.__cases[coord[0]][coord[1]]
     
+    # OK
     def setLongueur(self, longueur: int):
         self.__longueur = longueur
         
+    # OK
     def setLargeur(self, largeur: int):
         self.__largeur = largeur
     
+    # OK
     def regenererPlan(self):
         self.__cases = [[Case((i, j)) for i in range(self.__longueur)] for j in range(self.__largeur)]
     
+    # à tester
     def casesVoisines(self, case: Case):
         coord = case.getCoord()
         voisins = []
@@ -36,6 +44,7 @@ class Plan():
                 voisins.append(self.__cases[voisinCoord[0]][voisinCoord[1]])
         return voisins
     
+    # à tester
     def setItemsAccessible(self, coord: tuple):
         if 0 <= coord[0] < self.__largeur and 0 <= coord[1] < self.__longueur:
             case = self.__cases[coord[0]][coord[1]]
@@ -49,6 +58,7 @@ class Plan():
                             itemsAccessible.extend(caseAdj.getItemsInRayon())
             case.setItemsAccessible(itemsAccessible)
     
+    # à tester
     def remplirCase(self, coord: tuple):
         if 0 <= coord[0] < self.__largeur and 0 <= coord[1] < self.__longueur:
             case = self.__cases[coord[0]][coord[1]]
@@ -62,11 +72,13 @@ class Plan():
             case.setObstacle(input(f"Est-ce que la case {coord} est un obstacle ? (oui/non) ").strip().lower() == 'oui')
             self.setItemsAccessible(coord)
     
+    # à tester
     def remplirPlan(self):
         for i in range(self.__largeur):
             for j in range(self.__longueur):
                 self.remplirCase((i, j))
     
+    # à tester
     def trouverDepart(self):
         for i in range(self.__largeur):
             for j in range(self.__longueur):
@@ -75,6 +87,7 @@ class Plan():
                     return case
         return None
     
+    # à tester
     def trouverCasesPossiblesItem(self, item: str):
         casesPossibles = []
         for i in range(self.__largeur):
@@ -84,6 +97,7 @@ class Plan():
                     casesPossibles.append(case)
         return casesPossibles
     
+    # à revoir car case plus proche du départ != case plus proche d'une autre case ? à tester sinon
     def trouverCasesAVisiter(self, listeCourses: list):
         casesAVisiter = []
         depart = self.trouverDepart()
@@ -101,6 +115,7 @@ class Plan():
                 casesAVisiter.append(case)
         return casesAVisiter
     
+    # à tester et surement à revoir
     def plusCourtCheminCase(self, depart: tuple, arrivee: tuple):
         distances = {(i,j): float('inf') for i in range(self.__largeur) for j in range(self.__longueur)}
         distances[depart] = 0
@@ -137,6 +152,7 @@ class Plan():
         chemin.reverse()
         return chemin
     
+    # à relire
     def plusCourtCheminListeCourses(self, listeCourses: list):
         cheminTotal = []
         casesAVisiter = self.trouverCasesAVisiter(listeCourses)
